654c4c0aced5369cea42d1d4bb461e947bcbac48
Navraj Awasthi <awasthintr07@gmail.com>
2026-02-24 22:27:29 +0545

    solved some bugs from frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AddBook from "./pages/addBook/AddBook";
@@ -6,8 +5,6 @@ import EditBook from "./pages/editBook/EditBook";
 import Home from "./pages/home/Home";
 import SingleBook from "./pages/singleBook/SingleBook";
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
